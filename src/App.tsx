cd6814ea33a1e0ae53852b17820ecc931cfcb504
minhnghia-exe <minh-nghia@system-exe.com.vn>
2025-09-23 10:04:41 +0700

    feat: add React and MUI components
    - Updated package.json to include React, React DOM, MUI, Emotion, and styled-components.
    - Created App component to render Hello component.
    - Added Hello component that displays a heading using MUI Typography.
    - Updated renderer.ts to import App component.
    - Configured TypeScript to use React JSX.

---
+++ src/App.tsx
@@ -0,0 +1,6 @@
+import React from "react";
+import { createRoot } from "react-dom/client";
+import Hello from "./components/Hello";
+
+const root = createRoot(document.body);
+root.render(<Hello />);
---
+++ src/components/Hello.tsx
@@ -0,0 +1,12 @@
+import { Typography } from "@mui/material";
+import React from "react";
+
+const Hello = () => {
+  return (
+    <Typography variant="h3" component="h2">
+      h1. Heading
+    </Typography>
+  );
+};
+
+export default Hello;
