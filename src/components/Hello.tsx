cbdebf3abd9dd02a8f52c24fe82e97c1ea106a9c
minhnghia-exe <minh-nghia@system-exe.com.vn>
2025-09-23 10:23:42 +0700

    feat: update Typography variant in Hello component and add font imports in renderer

--- src/components/Hello.tsx
+++ src/components/Hello.tsx
@@ -3,7 +3,7 @@ import { Typography } from "@mui/material";
 
 const Hello = () => {
   return (
-    <Typography variant="h3" component="h2">
+    <Typography variant="h1" component="h2" fontWeight={700}>
       h1. Heading
     </Typography>
   );
--- src/renderer.ts
+++ src/renderer.ts
@@ -27,4 +27,10 @@
  */
 
 import './index.css';
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
+
 import "./index.tsx";
+
